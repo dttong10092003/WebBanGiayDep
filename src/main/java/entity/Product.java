2fb13aeabc0391573bc1068bb7bcec5e9a11884b
DESKTOP-BPE6J3K\DELL <tong10092003@gmail.com>
2024-06-29 21:56:19 +0700

    add entities

---
+++ src/main/java/entity/Product.java
@@ -0,0 +1,14 @@
+package entity;
+
+public class Product {
+	private String id;
+	private String name;
+	private String[] image;
+	private double price;
+	private String description;
+	private int quantity;
+	private Category categoryID;
+	private String color;
+	private int size;
+	
+}
